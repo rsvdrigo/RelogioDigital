bccfbc4eac497ca9c8a0742173afd1b340e0534a
rsvdrigo <rsvdrigo@gmail.com>
2025-09-17 13:39:12 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let secon = dateToday.getSeconds();
+
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) hr = '0' + min;
+    if (min < 10) hr = '0' + secon;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = secon;
+}
+
+)
